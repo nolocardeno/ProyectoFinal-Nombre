6c6b5bd4ffeb236b4a0ede3566ea02e605a7a8cf
nolorubio23 <mcarsan165@g.educaand.es>
2026-02-20 07:42:39 +0100

    Creado proyecto Springboot para el backend

---
+++ backend/src/test/java/com/nolocardeno/trophytracker/TrophyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolocardeno.trophytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrophyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
